942e6737ddf4198c180e6ba4f46418a51ac2d596
Ben Parisot <me@benparisot.com>
2019-05-22 17:34:08 -0700

    write actions for blog

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
+import { createPost, deletePost, CREATE_POST, DELETE_POST } from './actions/post-actions';
+import { nullLiteral } from '@babel/types';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  title: null,
+  body: null
+};
+
+const store = createStore(reducer);
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case CREATE_POST:
+      return { title: action.payload.title, body: action.payload.title };
+    case DELETE_POST: 
+      return { title: null, body: null };
+    default: 
+      return state;
+  }
+}
+
+console.log('initial store', store.getState());
+
+store.dispatch(createPost({ title: 'My First Blog Post', body: 'This is a very cool blog' }));
+console.log('added post', store.getState());
+
+store.dispatch(deletePost());
+console.log('deleted post', store.getState());
+
