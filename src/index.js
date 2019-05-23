fa1a1454245aba4b22bc36d5da47a91682b40bac
Ben Parisot <me@benparisot.com>
2019-05-22 17:07:45 -0700

    write and pass all tests

--- src/index.js
+++ src/index.js
@@ -1,65 +1,10 @@
 import { createStore } from 'redux';
-
-const initialState = {
-  drink: null,
-  chips: null,
-  sandwich: null
-};
-
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case 'ADD_DRINK':
-      return { ...state, drink: action.payload };
-    case 'REMOVE_DRINK':
-      return { ...state, drink: null };
-    case 'ADD_CHIPS':
-      return { ...state, chips: action.payload };
-    case 'REMOVE_CHIPS':
-      return { ...state, chips: null };
-    case 'ADD_SAMMIE':
-      return { ...state, sandwich: action.payload };
-    case 'REMOVE_SAMMIE':
-      return { ...state, sandwich: null };
-    case 'EMPTY-LUNCHBOX':
-      return { state };
-    default:
-      return state;
-  }
-}
+import { addDrink, removeDrink, addChips, removeChips, addSammie, removeSammie, emptyLunchbox } from './actions/lunch-actions';
+import reducer from '../src/reducers/lunchReducer';
 
 const store = createStore(reducer);
-const ADD_DRINK = 'ADD_DRINK';
-const ADD_CHIPS = 'ADD_CHIPS';
-const ADD_SAMMIE = 'ADD_SAMMIE';
-const REMOVE_DRINK = 'REMOVE_DRINK';
-const REMOVE_CHIPS = 'REMOVE_CHIPS';
-const REMOVE_SAMMIE = 'REMOVE_SAMMIE';
-const EMPTY_LUNCHBOX = 'EMPTY_LUNCHBOX';
 
-const addDrink = drink => ({
-  type: ADD_DRINK,
-  payload: drink
-});
-const addChips = chips => ({
-  type: ADD_CHIPS,
-  payload: chips
-});
-const addSammie = sammie => ({
-  type: ADD_SAMMIE,
-  payload: sammie
-});
-const removeDrink = () => ({
-  type: REMOVE_DRINK
-});
-const removeChips = () => ({
-  type: REMOVE_CHIPS
-});
-const removeSammie = () => ({
-  type: REMOVE_SAMMIE
-});
-const emptyLunchbox = () => ({
-  type: EMPTY_LUNCHBOX
-});
+
 
 console.log('initial store', store.getState());
 
--- tests/store.test.js
+++ tests/store.test.js
@@ -1,28 +1,32 @@
+import { addDrink, removeDrink, addChips, removeChips, addSammie, removeSammie } from '../src/actions/lunch-actions';
+import reducer from '../src/reducers/lunchReducer';
 import { createStore } from 'redux';
-
 describe('store test', () => {
-  it('adds to the store', () => {
-    const initialState = {
-      object: null
-    };
 
-    function reducer(state = initialState, action) {
-      switch(action.type) {
-        case 'ADD_OBJECT':
-          return { ...state, object: action.payload };
-        default: 
-          return state;
-      }
-    }
+  const store = createStore(reducer);
 
-    const store = createStore(reducer);
-
-    store.dispatch({
-      type: 'ADD_OBJECT',
-      payload: 'hot tamale'
-    });
-    
-    expect(store.getState()).toEqual({ object: 'hot tamale' });
-
+  it('adds a drink to the store', () => {
+    store.dispatch(addDrink('melk'));
+    expect(store.getState()).toEqual({ drink: 'melk', chips: null, sandwich: null });
   });
+
+  it('removes a drink from the store', () => {
+    store.dispatch(removeDrink());
+    expect(store.getState()).toEqual({ drink: null, chips: null, sandwich: null });  });
+
+  it('adds a chips to the store', () => {
+    store.dispatch(addChips('fritos'));
+    expect(store.getState()).toEqual({ drink: null, chips: 'fritos', sandwich: null });  });
+
+  it('removes a chips from the store', () => {
+    store.dispatch(removeChips());
+    expect(store.getState()).toEqual({ drink: null, chips: null, sandwich: null });  });
+
+  it('adds a sammie to the store', () => {
+    store.dispatch(addSammie('chicksen'));
+    expect(store.getState()).toEqual({ drink: null, chips: null, sandwich: 'chicksen' });  });
+
+  it('removes a sammie from the store', () => {
+    store.dispatch(removeSammie());
+    expect(store.getState()).toEqual({ drink: null, chips: null, sandwich: null });  });
 });
