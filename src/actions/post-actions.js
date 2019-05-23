ae0d2db02be4b6faf9a2190aa832bc212eb3774f
Ben Parisot <me@benparisot.com>
2019-05-22 20:04:47 -0700

    wip delete action

--- src/actions/post-actions.js
+++ src/actions/post-actions.js
@@ -1,11 +1,13 @@
 export const CREATE_POST = 'CREATE_POST';
 export const DELETE_POST = 'DELETE_POST';
+export const DELETED_NOTIFICATION = 'Post deleted';
 
 export const createPost = post => ({
   type: CREATE_POST,
   payload: post
 });
 
-export const deletePost = () => ({
-  type: DELETE_POST,
+export const deletePost = id => ({
+  type: DELETE_POST, 
+  payload: id
 });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import { createStore } from 'redux';
 import { createPost, deletePost, CREATE_POST, DELETE_POST } from './actions/post-actions';
-import { nullLiteral } from '@babel/types';
+// the post should have an id and a body, which has a title and a textBody
 
-const initialState = {
+const initialState = [{
   title: null,
   body: null
-};
-
+}];
+// push to initialstate 
 const store = createStore(reducer);
 
 function reducer(state = initialState, action) {
   switch(action.type) {
     case CREATE_POST:
-      return { title: action.payload.title, body: action.payload.title };
+      return [...state, { title: action.payload.title, body: action.payload.title }];
     case DELETE_POST: 
-      return { title: null, body: null };
+      return [...state, action.payload];
     default: 
       return state;
   }
@@ -25,6 +25,11 @@ console.log('initial store', store.getState());
 store.dispatch(createPost({ title: 'My First Blog Post', body: 'This is a very cool blog' }));
 console.log('added post', store.getState());
 
-store.dispatch(deletePost());
-console.log('deleted post', store.getState());
+store.dispatch(createPost({ title: 'My Second Blog Post', body: 'This is a very cool blog' }));
+console.log('added post', store.getState());
+
+store.dispatch(createPost({ title: 'My Third Blog Post', body: 'This is a very cool blog' }));
+console.log('added post', store.getState());
+// store.dispatch(deletePost(1));
+// console.log('deleted post', store.getState());
 
